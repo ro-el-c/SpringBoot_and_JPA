ebee0c143bcb88494d504f4cb62dab7949a5c0e4
ro-el <alcxzp@gmail.com>
2023-08-08 16:14:55 +0900

    1. Project Setting - Test

---
+++ src/main/java/jpa/shop/MemberRepository.java
@@ -0,0 +1,20 @@
+package jpa.shop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext // EntityManager 주입해줌
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
