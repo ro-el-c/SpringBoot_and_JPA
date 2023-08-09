f1fcd205835b843d9e0a2f3e24b317a4e87c5dd2
ro-el <alcxzp@gmail.com>
2023-08-09 16:59:39 +0900

    2. Domain Analysis Design - Entity Design Caution (fetch lazy)
    연관 관계는 지연 로딩으로 설정

--- src/main/java/jpa/shop/domain/Order.java
+++ src/main/java/jpa/shop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -16,14 +18,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 주문:회원 = N:1
+    @ManyToOne(fetch = LAZY) // 주문:회원 = N:1
+    // @XToOne 는 즉시 로딩 -> 지연 로딩 설정 ; FetchType.LAZY
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
