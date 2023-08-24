b973e2854ef113215eee87b59f0dc908ea1d4e9f
ro-el <alcxzp@gmail.com>
2023-08-25 02:20:02 +0900

    7. Web Hierarchy - Member List

--- src/main/java/jpa/shop/controller/MemberController.java
+++ src/main/java/jpa/shop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -41,5 +43,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
