4c6a4e5816252dad7f2e6c83045b06866a3cd50e
ro-el-c <chung3286@naver.com>
2023-08-26 02:36:56 +0900

    7. Web Hierarchy - Order

---
+++ src/main/java/jpa/shop/controller/OrderController.java
@@ -0,0 +1,43 @@
+package jpa.shop.controller;
+
+import jpa.shop.domain.Member;
+import jpa.shop.domain.item.Item;
+import jpa.shop.service.ItemService;
+import jpa.shop.service.MemberService;
+import jpa.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findAllMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
